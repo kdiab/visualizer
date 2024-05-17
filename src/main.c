170d4a0bd77490a208960c465ce050672a29ee1a
kdiab <khalid@diab.cc>
2024-05-17 18:56:30 -0400

    fix bug in max and segfault, allow user to pass in song, organize project

--- src/main.c
+++ src/main.c
@@ -9,9 +9,9 @@
 
 #define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
 
-#define N (1<<14)
-
 float pi;
+
+#define N 256
 float in[N];
 float complex out[N];
 float max;
@@ -21,9 +21,9 @@ typedef struct {
     float right;
 } Frame;
 
-
-void fft(float in[], size_t stride, float complex out[], size_t n) {
-
+// Ported from https://rosettacode.org/wiki/Fast_Fourier_transform#Python
+void fft(float in[], size_t stride, float complex out[], size_t n)
+{
     assert(n > 0);
 
     if (n == 1) {
@@ -31,31 +31,31 @@ void fft(float in[], size_t stride, float complex out[], size_t n) {
         return;
     }
 
-    fft(in, stride*2, out, n/2); // even
-    fft(in + stride, stride*2, out + n/2, n/2); // odd
-    
-    // out = [ e e e e | o o o o ]
+    fft(in, stride*2, out, n/2);
+    fft(in + stride, stride*2,  out + n/2, n/2);
 
     for (size_t k = 0; k < n/2; ++k) {
         float t = (float)k/n;
         float complex v = cexp(-2*I*pi*t)*out[k + n/2];
         float complex e = out[k];
-        out[k] = e + v;
+        out[k]       = e + v;
         out[k + n/2] = e - v;
     }
 }
 
-float amp(float complex z) {
+float amp(float complex z)
+{
     float a = fabsf(crealf(z));
     float b = fabsf(cimagf(z));
     if (a < b) return b;
     return a;
 }
 
-void callback(void *bufferData, unsigned int frames) {
-    if (frames < N) return;
+void callback(void *bufferData, unsigned int frames)
+{
+    if (frames > N) frames = N;
 
-    Frame *fs= bufferData;
+    Frame *fs = bufferData;
 
     for (size_t i = 0; i < frames; ++i) {
         in[i] = fs[i].left;
@@ -70,16 +70,43 @@ void callback(void *bufferData, unsigned int frames) {
     }
 }
 
-int main(void) {
-    
+char *shift_args(int *argc, char ***argv)
+{
+    assert(*argc > 0);
+    char *result = (**argv);
+    (*argv) += 1;
+    (*argc) -= 1;
+    return result;
+}
+
+int main(int argc, char **argv)
+{
     pi = atan2f(1, 1)*4;
 
-    InitWindow(800, 600, "Music");
+    const char *program = shift_args(&argc, &argv);
+
+    if (argc == 0) {
+        fprintf(stderr, "Usage: %s <input>\n", program);
+        fprintf(stderr, "ERROR: no input file is provided\n");
+        return 1;
+    }
+    const char *file_path = shift_args(&argc, &argv);
+
+    InitWindow(800, 600, "Musializer");
     SetTargetFPS(60);
 
     InitAudioDevice();
-    Music music = LoadMusicStream("ogi-feel-the-beat-expanded-191267.mp3");
+
+    Music music = LoadMusicStream(file_path);
+    printf("music.frameCount = %u\n", music.frameCount);
+    printf("music.stream.sampleRate = %u\n", music.stream.sampleRate);
+    printf("music.stream.sampleSize = %u\n", music.stream.sampleSize);
+    printf("music.stream.channels = %u\n", music.stream.channels);
+//    assert(music.stream.sampleSize == 16);
+//    assert(music.stream.channels == 2);
+
     PlayMusicStream(music);
+    SetMusicVolume(music, 0.5f);
     AttachAudioStreamProcessor(music.stream, callback);
 
     while (!WindowShouldClose()) {
@@ -92,7 +119,7 @@ int main(void) {
                 ResumeMusicStream(music);
             }
         }
-        
+
         int w = GetRenderWidth();
         int h = GetRenderHeight();
 
@@ -101,9 +128,10 @@ int main(void) {
         float cell_width = (float)w/N;
         for (size_t i = 0; i < N; ++i) {
             float t = amp(out[i])/max;
-            DrawRectangle(i*cell_width, h/2 - h/2*t, cell_width, h/2*t, BLUE);
+            DrawRectangle(i*cell_width, h/2 - h/2*t, cell_width, h/2*t, RED);
         }
         EndDrawing();
     }
+
     return 0;
 }
