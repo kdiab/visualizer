98650d7e3bf2bff88d9e2801ac691a6a9134251b
kdiab <khalid@diab.cc>
2024-05-21 19:51:12 -0400

    time and stuff

--- src/fft.c
+++ src/fft.c
@@ -44,7 +44,7 @@ int main(void) {
     
     pi = atan2f(1, 1) * 4; // fortran trickery
     
-    size_t n = 32;
+    size_t n = (1<<16);
     float in[n];
     float complex out[n];
 
@@ -54,34 +54,19 @@ int main(void) {
          in[i] = cosf(2*pi*t) + sinf(2*pi*t*2);
     }
 
+#if 0
+    printf("FFT\n");
     fft(in, 1, out, n);
 
-    for (size_t f = 0; f < n; ++f) {
-        printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
-    }
+//    for (size_t f = 0; f < n; ++f) {
+//        printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
+//    }
+#else
+    printf("DFT\n");
+    dft(in, out, n);
 
-#if 0
-    for (size_t f = 0; f < n/2; ++f) {
-        out[f] = 0;
-        out[f + n/2] = 0;
-
-        // even
-        for (size_t i = 0; i < n; i += 2) {
-            float t = (float)i/n;
-            float complex v = in[i]*cexp(2*I*pi*t*f);
-            out[f] += v;
-            out[f + n/2] +=v;
-        }
-
-        //odd
-        for (size_t i = 1; i < n; i += 2) {
-            float t = (float)i/n;
-            float complex v = in[i]*cexp(2*I*pi*t*f);
-            out[f] += v;
-            out[f + n/2] -=v;
-        }
-
-    }
+//    for (size_t f = 0; f < n; ++f) {
+//        printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
+//    }
 #endif
-
 }
